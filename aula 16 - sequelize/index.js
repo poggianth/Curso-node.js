4bad5ea8f08cf051eb3bd2ab07194e05a44dc509
Thiago Melo Poggian <poggianth@gmail.com>
2023-07-10 19:52:37 -0300

    list and delete postagens (mySql)

--- aula 16 - sequelize/index.js
+++ aula 16 - sequelize/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-import { engine } from 'express-handlebars';
+import { engine as handlebars } from 'express-handlebars';
 import bodyParser from 'body-parser';
 import {Post} from "./models/post.js";
 var port = 8081;
@@ -8,7 +8,7 @@ var port = 8081;
 
 // Config
     // Template Engine
-    app.engine('handlebars', engine({defaultLayout: 'main'}));
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}));
     app.set('view engine', 'handlebars');
 
     // Body Parser
@@ -19,7 +19,11 @@ var port = 8081;
 // Rotas
 
 app.get('/', (req, res) => {
-    res.render('home');
+    // Busca os dados no banco de dados
+    // Atributo order: indica o campo onde ordenar e como vai ordernar
+    Post.findAll({order: [['id', 'DESC']]}).then((posts) => {
+        res.render('home', {posts: posts});
+    });
 });
 
 app.get('/cadastro', (req, res) => {
@@ -37,6 +41,15 @@ app.post('/add', (req, res) => {
         res.redirect('/');
     }).catch((erro) => {
         res.send(`[OPS] Erro ao cadastrar um Post: ${erro}`);
+    });
+});
+
+app.get('/deletar/:id', (req, res) => {
+    // destroy = deleta no DB
+    Post.destroy({where: {'id': req.params.id}}).then(() => {
+        res.send("Postagem deletada com sucesso!");
+    }).catch((error) => {
+        res.send(`Erro ao excluir postagem: ${error}`);
     });
 });
 
